14d381025a347c55704dcae299dedfdac4818d44
paripb <paripb@gmail.com>
2017-02-04 16:19:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-
-?>
+<!doctype html>
+<title>ThySignup</Title>
+<head><h1>ThyTalent</h1></head>
+<form>
+<fieldset>
+<legend>SignUp</legend>
+Email<br>
+<input type="text" name="email"><br>
+Password<br>
+<input type="text" name="password"><br>
+Confirm Password<br>
+<input type="text"><br>
+<input type="submit" value = "Submit">
+<input type="reset" value = "Reset">
+</fieldset>
+</form>
